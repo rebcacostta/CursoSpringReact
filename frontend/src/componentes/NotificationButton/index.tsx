d20ead424e1134f7714f2bf0584ee092d17bd1ae
Rebeca <rbcmarilia100@gmail.com>
2022-09-09 15:37:09 -0300

    Adicionando o useStage para manter as datas selecionadas

--- frontend/src/componentes/NotificationButton/index.tsx
+++ frontend/src/componentes/NotificationButton/index.tsx
@@ -1,5 +1,5 @@
-import icon  from '../../assets/img/Vector.svg'
-import './styles.css'
+import icon  from '../../assets/img/Vector.svg';
+import './styles.css';
 
 function NotificationButton() {
     return (
@@ -11,4 +11,4 @@ function NotificationButton() {
     )
   }
   
-  export default NotificationButton
+  export default NotificationButton;
